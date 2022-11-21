c5b5fa64ff33a550f35d98037f4b21e3ffe84bbd
Matej Furmanek <matej.furmanek@gjensidige.no>
2022-11-21 16:06:17 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './main/styles/index.css';
+import App from './main/components/App';
+import reportWebVitals from './main/hooks/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
